b9f8ce1d9ea0fe80bc4c256f765feef1d0c60267
Ruairí Glackin <ruairiglackin@gmail.com>
2025-03-07 23:51:06 +0000

    integrated with flask logger

--- app.py
+++ app.py
@@ -8,14 +8,15 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
-
+setup_logging()
+app.logger.handlers = logging.getLogger().handlers
+app.logger.setLevel(logging.DEBUG)
 app.register_blueprint(main_bp)
 
 if __name__ == '__main__':
-    setup_logging()
+    
     with app.app_context():
         db.create_all()
-        logging.info("Database Setup Successfully")
+        app.logger.info("Database Setup Successfully")
         
     app.run(debug=True)
-    logging.info("Application Completed Successfully") 
