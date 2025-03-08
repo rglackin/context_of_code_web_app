e886b4aedf90ce388838998613d095493fd24a4b
Ruairí Glackin <ruairiglackin@gmail.com>
2025-03-08 16:43:18 +0000

    update app.py to create db on PA

--- app.py
+++ app.py
@@ -18,18 +18,22 @@ setup_logging()
 
 app.logger.handlers = logging.getLogger().handlers
 app.logger.setLevel(logging.DEBUG)
+
+
+with app.app_context():
+        inspector = db.inspect(db.engine)
+        tables = inspector.get_table_names()
+        app.logger.debug(f"Tables before creation: {tables}")
+        
+        db.create_all()
+        
+        app.logger.debug(f"Tables after creation: {tables}")
+        
+        app.logger.info("Database Setup Successfully")
+
 app.register_blueprint(main_bp)
 
 app.logger.info("App Setup Successfully")
-
+        
 if __name__ == '__main__':
-    
-    with app.app_context():
-        inspector = db.inspect(db.engine)
-        tables = inspector.get_table_names()
-        app.logger.info(f"Tables: {tables}")
-        
-        db.create_all()
-        app.logger.info("Database Setup Successfully")
-        
     app.run()
